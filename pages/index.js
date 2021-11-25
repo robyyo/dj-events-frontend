0780cd272dbf85cdd18db09c7419671f3cf2605f
Robert Finkley <me@robertfinkley.com>
2021-11-25 15:09:31 -0500

    limited number of events on HomePage

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import Layout from '@/components/Layout';
+import Link from 'next/link';
 import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 
 export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -14,7 +25,7 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events },
+    props: { events: events.events.slice(0, 3) },
     revalidate: 1,
   };
 }
