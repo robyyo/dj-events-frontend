51e6fda774b460cadfc644b49a7b992937d7b9ea
Robert Finkley <me@robertfinkley.com>
2021-11-27 11:01:12 -0500

    removed console log of events

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { API_URL } from '@/config/index';
 import EventItem from '@/components/EventItem';
 
 export default function HomePage({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>Upcoming Events</h1>
@@ -23,7 +24,6 @@ export default function HomePage({ events }) {
 export async function getStaticProps() {
   const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
   const events = await res.json();
-
   return {
     props: { events },
     revalidate: 1,
