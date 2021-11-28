990c9abfafcae24684137b5b18e6b274ba0946ad
Robert Finkley <me@robertfinkley.com>
2021-11-28 09:24:45 -0500

    removed console log

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import { API_URL } from '@/config/index';
 import EventItem from '@/components/EventItem';
 
 export default function HomePage({ events }) {
-  console.log(events);
   return (
     <Layout>
       <h1>Upcoming Events</h1>
