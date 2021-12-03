15ee1b8d883c19dccd7e5615a9929e3f7f9d1887
Robert Finkley <me@robertfinkley.com>
2021-12-03 13:10:34 -0500

    added AuthProvider

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
 import Link from 'next/link';
 import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.css';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 
 function Header() {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -19,19 +22,39 @@ function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                <FaSignInAlt />
-                Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              {' '}
+              <li>
+                <Link href="/events/add">
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/account/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  className="btn-secondary btn-icon"
+                  onClick={() => logout()}
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
