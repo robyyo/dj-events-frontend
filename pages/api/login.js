08277b6d6500ce01dab655c31ecbdcbf2b2eb432
Robert Finkley <me@robertfinkley.com>
2021-12-04 11:09:20 -0500

    Set cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
 import { API_URL } from '@/config/index';
+import cookie from 'cookie';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
@@ -18,6 +19,16 @@ export default async (req, res) => {
     console.log(data);
     if (strapiRes.ok) {
       // set cookie
+      res.setHeader(
+        'Set-Cookie',
+        cookie.serialize('token', data.jwt, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV !== 'development',
+          maxAge: 60 * 60 * 24 * 7, // 1 week,
+          sameSite: 'strict',
+          path: '/'
+        })
+      );
       res.status(200).json({ user: data.user });
     } else {
       res
