98a43acdf6789b9eba9fc6861ec93488084a5b76
Robert Finkley <me@robertfinkley.com>
2021-12-05 12:39:34 -0500

    changed back to getServerSideProps

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -57,28 +57,30 @@ function EventPage({ evt }) {
   );
 }
 
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/events`);
-  const events = await res.json();
-  const paths = events.map((evt) => ({ params: { slug: evt.slug } }));
-  return { paths, fallback: true };
-}
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events`);
+//   const events = await res.json();
+//   const paths = events.map((evt) => ({ params: { slug: evt.slug } }));
+//   return { paths, fallback: true };
+// }
 
-export async function getStaticProps({ params: { slug } }) {
+// export async function getStaticProps({ params: { slug } }) {
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`);
+//   const events = await res.json();
+//   return {
+//     props: { evt: events[0] },
+//     revalidate: 1,
+//   };
+// }
+
+export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/events?slug=${slug}`);
   const events = await res.json();
   return {
-    props: { evt: events[0] },
-    revalidate: 1,
+    props: {
+      evt: events[0],
+    },
   };
 }
-
-// export async function getServerSideProps({ query: { slug } }) {
-//   const res = await fetch(`${API_URL}/api/events/${slug}`);
-//   const events = await res.json();
-//   return {
-//     props: { evt: events.evt[0] },
-//   };
-// }
 
 export default EventPage;
