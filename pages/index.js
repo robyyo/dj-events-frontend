95256ffba12adb04a5036e5b4667eea2d37e3301
Robert Finkley <me@robertfinkley.com>
2021-12-06 12:02:51 -0500

    testing events

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export async function getStaticProps() {
   const res = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=3`);
   const events = await res.json();
   return {
-    props: events,
+    props: { events },
     revalidate: 1,
   };
 }
